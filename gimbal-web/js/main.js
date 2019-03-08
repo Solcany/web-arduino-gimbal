d85dd77a7c4c557bbdd796904399558fef2ac38e
Unknown <solcany.m@gmail.com>
2019-03-08 09:07:07 +0100

    web layout

--- gimbal-web/js/main.js
+++ gimbal-web/js/main.js
@@ -3,11 +3,15 @@
 let w = window.innerWidth;
 let h = window.innerHeight;
 
+var yaw = 0;
+var pitch, roll;
 
-let cube;
+let axisArmLength = 100;
+let axisoff = 25;
+let iphone;
 
 function preload() {
-  cube = loadModel("/models/cube.obj")
+  iphone = loadModel("/models/iphone5.obj")
 }
 
 
@@ -17,10 +21,28 @@ function setup() {
 }
 
 function draw() {
-	background(255, 0, 0);
+	clear();
 	push();
-	rotateY(millis() / 250)
-	model(cube);
+	translate(250, 0, 0)
+	rotateZ(mouseX)
+
+
+	strokeWeight(3.0);
+	stroke(255,0,0);
+	line(-axisArmLength,0,axisoff, axisArmLength,0,axisoff)
+	stroke(0,255,0);
+	line(0,-axisArmLength,axisoff, 0,axisArmLength,axisoff)
+	stroke(0,0,255);
+	line(0,0, -axisArmLength, 0,0,axisArmLength)	
+
+
+	stroke(0);
+	strokeWeight(1);
+	scale(4.0);
+	fill('#ffeb16');
+	model(iphone);
+
+
 	pop();
 	// translate(100, 0, 0);
 }
@@ -31,7 +53,7 @@ window.onload = function() {
 
 
 
-  var client = mqtt.connect('mqtt://msolcany~nodemcu@broker.shiftr.io', {
+  var client = mqtt.connect('mqtt://msolcany~ypr@broker.shiftr.io', {
     clientId: 'browser-local'
   });
     
@@ -45,10 +67,51 @@ window.onload = function() {
     console.log('client has connected!');
   }); 
 
-  client.subscribe('nodemcu/vals');
+  client.subscribe('ypr/yaw');
 
   client.on('message', function(topic, message) {
-    document.getElementById('vals_content').innerHTML = message;
-    console.log(message);
+    yaw = Math.round(Utf8ArrayToStr(message));
+    console.log(yaw);
   })
+
+
+  function easeToVal(currVal, newVal, time) {
+  	var diff = currVal - newVal;
+
+  }
+
+
+  function Utf8ArrayToStr(array) {
+	    var out, i, len, c;
+	    var char2, char3;
+
+	    out = "";
+	    len = array.length;
+	    i = 0;
+	    while(i < len) {
+		c = array[i++];
+		switch(c >> 4)
+		{ 
+		  case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+		    // 0xxxxxxx
+		    out += String.fromCharCode(c);
+		    break;
+		  case 12: case 13:
+		    // 110x xxxx   10xx xxxx
+		    char2 = array[i++];
+		    out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+		    break;
+		  case 14:
+		    // 1110 xxxx  10xx xxxx  10xx xxxx
+	        char2 = array[i++];
+		    char3 = array[i++];
+		    out += String.fromCharCode(((c & 0x0F) << 12) |
+						   ((char2 & 0x3F) << 6) |
+						   ((char3 & 0x3F) << 0));
+		    break;
+		}
+	    }
+
+	    return out;
+	}
 }
